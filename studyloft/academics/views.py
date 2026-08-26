3d21d536f4dfc94f6786abebbd0f270f0a64b9d6
vuca1 <malihavita@gmail.com>
2026-08-26 12:57:14 +0200

    first commit

---
+++ studyloft/academics/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    pass
+
+
+def register(request):
+    pass
+
+
+def login(request):
+    pass
+
+
+def logout():
+    pass
