ca64b4920f2e221c7c96cadb500c51b17cefafc8
vuca1 <malihavita@gmail.com>
2026-08-26 13:23:54 +0200

    Implemented login function in views.py

--- studyloft/academics/views.py
+++ studyloft/academics/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django import forms
 
 # Create your views here.
 def index(request):
@@ -10,7 +13,25 @@ def register(request):
 
 
 def login(request):
-    pass
+    if request.method == "POST":
+
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # check if authenticate successful
+        if user is not None:
+            login(request, user)
+            return render(request, "academics/index.html", {
+                "message": "Login successful."
+            })
+        else:
+            return render(request, "academics/login.html", {
+                "message": "Invalid username and/or password."
+            })
+
+    else:
+        return render(request, "academics/login.html")
 
 
 def logout():
